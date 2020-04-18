4b5e4658336e34639fc907bef6c26a6f10187b19
Hugo Sanchez <hugo.sanchez.pro@gmail.com>
2020-04-18 17:06:30 +0200

    FV-002: Add Entity and MariaDB Connection

---
+++ FreshVotes/src/main/java/domain/CommentId.java
@@ -0,0 +1,29 @@
+package domain;
+
+import javax.persistence.Embeddable;
+import javax.persistence.ManyToOne;
+@Embeddable
+public class CommentId {
+
+	@ManyToOne
+	private User user;
+	@ManyToOne
+	private Feature feature;
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	public Feature getFeature() {
+		return feature;
+	}
+
+	public void setFeature(Feature feature) {
+		this.feature = feature;
+	}
+
+}
