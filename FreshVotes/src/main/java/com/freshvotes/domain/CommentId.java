6c16344c6669ec3f609fb8b29634cf5f0ded09c1
Hugo Sanchez <hugo.sanchez.pro@gmail.com>
2020-04-18 18:04:37 +0200

    FV-002: Move Spring to v.2.1.14.BUILD-SNAPSHOT (Tables were not create)

--- FreshVotes/src/main/java/com/freshvotes/domain/CommentId.java
+++ FreshVotes/src/main/java/com/freshvotes/domain/CommentId.java
@@ -1,10 +1,13 @@
 package com.freshvotes.domain;
 
+import java.io.Serializable;
+
 import javax.persistence.Embeddable;
 import javax.persistence.ManyToOne;
 @Embeddable
-public class CommentId {
+public class CommentId implements Serializable {
 
+	private static final long serialVersionUID = 1L;
 	@ManyToOne
 	private User user;
 	@ManyToOne
