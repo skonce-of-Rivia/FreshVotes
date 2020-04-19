7fb50e161beb3d9fd691fd2b72583a15231b07bb
Hugo Sanchez <hugo.sanchez.pro@gmail.com>
2020-04-19 09:30:22 +0200

    FV-004: Spring Security Configuration

--- FreshVotes/src/main/java/com/freshvotes/domain/DashboardController.java
+++ FreshVotes/src/main/java/com/freshvotes/domain/DashboardController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class DashboardController {
 
+	 
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String rootView() {
 		return "index";
