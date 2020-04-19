a064946366fe27a770f3b60caae6a8232b3dfe02
Hugo Sanchez <hugo.sanchez.pro@gmail.com>
2020-04-19 08:57:14 +0200

    master: Merge FV-003

---
+++ FreshVotes/src/main/java/com/freshvotes/domain/Vote.java
@@ -0,0 +1,22 @@
+package com.freshvotes.domain;
+
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "t_vote")
+public class Vote {
+
+	@EmbeddedId
+	private VoteId primaryKey;
+	private Boolean upvote;
+
+	public Boolean getUpvote() {
+		return upvote;
+	}
+
+	public void setUpvote(Boolean upvote) {
+		this.upvote = upvote;
+	}
+}
